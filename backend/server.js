b2ea544efe03249f0df0b4a4202db1b2eefeed72
Shane Harper <cdtharper@gmail.com>
2021-12-26 10:02:30 -0500

    adding backend files. Need to get authentication to work

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const api = require('./backend/routes/Api');
-const passport = require("passport");
+const api = require('./routes/Api');
+// const passport = require("passport");
 const path = require('path');
 
 const db = require('./config/keys').mongoURI;
@@ -13,3 +13,11 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json({type: 'application/*+json'}));
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose
+    .connect(
+        process.env.MONGODB_URI || db,
+        {useNewUrlParser: true}
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
